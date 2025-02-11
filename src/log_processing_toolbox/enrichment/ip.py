db7cdeceb27aaaa57af1da146efd9c35df05adee
pbusenius <patrick.busenius@gmail.com>
2025-02-11 15:14:03 +0100

    ruff format

--- src/log_processing_toolbox/enrichment/ip.py
+++ src/log_processing_toolbox/enrichment/ip.py
@@ -8,36 +8,43 @@ from polars_maxminddb import (
 )
 
 
-def city_information(df: pl.DataFrame, ip_column: str = "id.orig_h", location_file: str = "data/GeoLite2-City.mmdb") -> pl.DataFrame:
-
+def city_information(
+    df: pl.DataFrame,
+    ip_column: str = "id.orig_h",
+    location_file: str = "data/GeoLite2-City.mmdb",
+) -> pl.DataFrame:
     print(location_file)
     return df.with_columns(
-        ip_lookup_city(df[ip_column], location_file).alias(
-            "city_information"
-        )
+        ip_lookup_city(df[ip_column], location_file).alias("city_information")
     )
 
 
 def location_information(
-    df: pl.DataFrame, ip_column: str = "id.orig_h", location_file: str = "data/GeoLite2-City.mmdb"
+    df: pl.DataFrame,
+    ip_column: str = "id.orig_h",
+    location_file: str = "data/GeoLite2-City.mmdb",
 ) -> pl.DataFrame:
     return df.with_columns(
-        ip_lookup_longitude(df[ip_column], location_file).alias(
-            "longitude"
-        ),
+        ip_lookup_longitude(df[ip_column], location_file).alias("longitude"),
         ip_lookup_latitude(df[ip_column], location_file).alias("latitude"),
     )
 
 
-def country_information(df: pl.DataFrame, ip_column: str = "id.orig_h", location_file: str = "data/GeoLite2-Country.mmdb") -> pl.DataFrame:
+def country_information(
+    df: pl.DataFrame,
+    ip_column: str = "id.orig_h",
+    location_file: str = "data/GeoLite2-Country.mmdb",
+) -> pl.DataFrame:
     return df.with_columns(
-        ip_lookup_country(df[ip_column], location_file).alias(
-            "country_information"
-        )
+        ip_lookup_country(df[ip_column], location_file).alias("country_information")
     )
 
 
-def asn_information(df: pl.DataFrame, ip_column: str = "id.orig_h", location_file: str = "data/GeoLite2-ASN.mmdb") -> pl.DataFrame:
+def asn_information(
+    df: pl.DataFrame,
+    ip_column: str = "id.orig_h",
+    location_file: str = "data/GeoLite2-ASN.mmdb",
+) -> pl.DataFrame:
     return df.with_columns(
         ip_lookup_asn(df[ip_column], location_file).alias("asn_information")
     )
