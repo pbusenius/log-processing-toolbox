7b456d3d27576845fe7aafa571ec492eb0e661f6
pbusenius <patrick.busenius@gmail.com>
2025-02-11 14:55:35 +0100

    added simple ssh example

---
+++ example/ssh/brute_force_graph.py
@@ -0,0 +1,24 @@
+from log_processing_toolbox.analysis import ssh
+from log_processing_toolbox.source.os import ssh as ssh_zeek_source
+from log_processing_toolbox.enrichment import ip
+
+
+def main():
+    # source
+    zeek_df = ssh_zeek_source.open_log("data/auth.log")
+
+    # analysis
+    df_brute_force = ssh.brute_force_detection(zeek_df)
+
+    # enrichment
+    df_brute_force = ip.city_information(df_brute_force)
+    # df_brute_force = ip.country_information(df_brute_force)
+    # df_brute_force = ip.asn_information(df_brute_force)
+    # df_brute_force = ip.location_information(df_brute_force)
+
+    # visualization#
+    print(df_brute_force)
+
+
+if __name__ == "__main__":
+    main()
