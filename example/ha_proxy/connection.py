e2e95edcad815946ad02d913dac8a3c31a6993dd
pbusenius <patrick.busenius@gmail.com>
2025-04-01 17:55:42 +0200

    added ha proxy source

---
+++ example/ha_proxy/connection.py
@@ -0,0 +1,22 @@
+import polars as pl
+
+from log_processing_toolbox.enrichment import ip
+from log_processing_toolbox.source.os import ha_proxy
+
+
+def main():
+    # source
+    connection_df = ha_proxy.open_log("data/haproxy.log")
+
+    # enrichment
+    connection_df = ip.city_information(connection_df)
+    connection_df = ip.country_information(connection_df)
+    connection_df = ip.asn_information(connection_df)
+
+    print(connection_df)
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/log_processing_toolbox/source/os/ha_proxy.py
@@ -0,0 +1,39 @@
+import re
+import polars as pl
+
+auth_log_regex = re.compile(
+    r"(?P<date>\S*\s*\d*\s\d{2}:\d{2}:\d{2})\s(?P<user>\S*)\s(?P<service>\S*)\sConnect\sfrom\s(?P<from_ip>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}):(?P<from_port>\d*)\sto\s(?P<to_ip>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}):(?P<to_port>\d*)"
+)
+
+
+def cast_columns(df: pl.DataFrame) -> pl.DataFrame:
+    return df.with_columns(
+        pl.col("ts").str.to_datetime("%Y %b  %d %H:%M:%S"),
+    )
+
+
+def open_log(file: str) -> pl.DataFrame:
+    data = {
+        "ts": [],
+        "id.orig_h": [],
+        "id.orig_p": [],
+        "id.resp_h": [],
+        "id.resp_p": [],
+    }
+
+    with open(file, "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            x = auth_log_regex.match(line)
+            if x is not None:
+                data["id.resp_h"].append(x["to_ip"])
+                data["id.resp_p"].append(x["to_port"])
+                data["id.orig_h"].append(x["from_ip"])
+                data["id.orig_p"].append(x["from_port"])
+                data["ts"].append(x["date"])
+
+    df = pl.DataFrame(data)
+
+    # df = cast_columns(df)
+
+    return df
