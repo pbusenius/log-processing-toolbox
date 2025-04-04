15e46e5f73380d1dab6f9e72263dac0b91c85b7e
pbusenius <patrick.busenius@gmail.com>
2025-04-04 10:04:33 +0200

    added example data

--- example/ssh/journal.py
+++ example/ssh/journal.py
@@ -3,17 +3,14 @@ from log_processing_toolbox.source.os import ssh as ssh_source
 
 
 def main():
-    ssh_df = ssh_source.open_journal_log("/media/pbusenius/One Touch/logs/146.70.101.118/journal")
+    ssh_df = ssh_source.open_journal_log("data/journal")
 
-    ssh_df.write_csv("146.70.101.118_ssh.csv")
+    ssh_df.write_csv("146.70.125.123_ssh.csv")
 
     # filter
     ssh_df = ssh_df.filter(
         pl.col("auth_success") == "T"
     )
-
-
-
 
 
 if __name__ == "__main__":
