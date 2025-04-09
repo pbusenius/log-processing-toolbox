1bdb00dd8773f251cb05f63f1978a7b358cb91b9
pbusenius <patrick.busenius@gmail.com>
2025-04-09 14:35:32 +0200

    added simple tor journalctl example

---
+++ example/tor/journal.py
@@ -0,0 +1,10 @@
+import polars as pl
+from log_processing_toolbox.source.os import tor as tor_source
+
+
+def main():
+    tor_df = tor_source.open_journal_log("data/journal")
+    
+
+if __name__ == "__main__":
+    main()
