0f2621685c0f7b48415d256ab4e28aa96957d58b
pbusenius <patrick.busenius@gmail.com>
2025-05-02 10:11:01 +0200

    added city, country, asn aggregation

--- example/open_vpn/journal.py
+++ example/open_vpn/journal.py
@@ -9,16 +9,21 @@ def main():
         "/media/pbusenius/ShadowCape/tmp/openvpn_logs/log/journal"
     )
 
-    vpn_df = vpn_df.group_by("id.orig_h").agg(
-        pl.col("ts").count().alias("number_of_log_entries"),
-        pl.col("ts").first().alias("first_timestamp"),
-        pl.col("ts").last().alias("last_timestamp"),
-    )
-
     # enrichment
     vpn_df = ip.city_information(vpn_df)
     vpn_df = ip.country_information(vpn_df)
     vpn_df = ip.asn_information(vpn_df)
+
+    vpn_df.write_csv("open_vpn.csv")
+
+    vpn_df = vpn_df.group_by(["id.orig_h", "client"]).agg(
+        pl.col("ts").count().alias("number_of_log_entries"),
+        pl.col("ts").first().alias("first_timestamp"),
+        pl.col("ts").last().alias("last_timestamp"),
+        pl.col("city_information").first(),
+        pl.col("country_information").first(),
+        pl.col("asn_information").first(),
+    )
 
     # export
     vpn_df.write_csv("openvpn_ips.csv")
